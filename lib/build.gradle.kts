d587ba3165593e9216922213573a38863b8a2799
detachhead <detachhead@users.noreply.github.com>
2025-04-30 20:43:40 +1000

    add ui with compose

---
+++ lib/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+  alias(libs.plugins.kotlin)
+  alias(libs.plugins.powerAssert)
+  alias(libs.plugins.ktfmt)
+  alias(libs.plugins.detekt)
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+  implementation(libs.directoryWatcher)
+  implementation(libs.coroutines)
+  testImplementation(libs.coroutinesTest)
+  testImplementation(kotlin("test"))
+}
+
+tasks.test { useJUnitPlatform() }
+
+kotlin {
+  jvmToolchain(16)
+  explicitApi()
+}
