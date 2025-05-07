fd80a75c0d80391b2112650799c01d461a3ed5cc
detachhead <detachhead@users.noreply.github.com>
2025-05-07 19:07:50 +1000

    add tooltips to watch folder and file buttons and fix watch file icon

---
+++ ui/src/main/kotlin/io/github/detachhead/indexer/ui/IconButtonWithTooltip.kt
@@ -0,0 +1,54 @@
+package io.github.detachhead.indexer.ui
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.TooltipArea
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun IconButtonWithTooltip(
+    onClick: () -> Unit,
+    icon: ImageVector,
+    tooltip: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    interactionSource: MutableInteractionSource? = null,
+) {
+
+  TooltipArea(
+      tooltip = {
+        Surface(
+            modifier = Modifier.shadow(4.dp),
+            color = Color(255, 255, 210),
+            shape = RoundedCornerShape(4.dp)) {
+              Text(text = tooltip, modifier = Modifier.padding(10.dp))
+            }
+      },
+      //      modifier = Modifier.padding(start = 40.dp),
+      delayMillis = 600,
+  ) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = colors,
+        interactionSource = interactionSource) {
+          Icon(icon, tooltip)
+        }
+  }
+}
