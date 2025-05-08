ed6f3be69097b3264f522eca2771e03095a992f2
detachhead <detachhead@users.noreply.github.com>
2025-05-08 19:42:24 +1000

    add new cutting edge AI powered search experience

---
+++ ui/src/main/kotlin/io/github/detachhead/indexer/ui/Icons.kt
@@ -0,0 +1,107 @@
+package io.github.detachhead.indexer.ui
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+/** from https://composeicons.com/icons/vscode-codicon/sparkle-filled */
+val sparkle by lazy {
+  ImageVector.Builder(
+          name = "SparkleFilled",
+          defaultWidth = 16.dp,
+          defaultHeight = 16.dp,
+          viewportWidth = 16f,
+          viewportHeight = 16f)
+      .apply {
+        path(
+            fill = SolidColor(Color(0xFF000000)),
+            fillAlpha = 1.0f,
+            stroke = null,
+            strokeAlpha = 1.0f,
+            strokeLineWidth = 1.0f,
+            strokeLineCap = StrokeCap.Butt,
+            strokeLineJoin = StrokeJoin.Miter,
+            strokeLineMiter = 1.0f,
+            pathFillType = PathFillType.NonZero) {
+              moveTo(5.398f, 10.807f)
+              curveTo(5.574f, 10.931f, 5.785f, 10.998f, 6f, 10.997f)
+              curveTo(6.216f, 10.998f, 6.427f, 10.93f, 6.602f, 10.804f)
+              curveTo(6.78f, 10.674f, 6.915f, 10.494f, 6.989f, 10.286f)
+              lineTo(7.436f, 8.913f)
+              curveTo(7.551f, 8.569f, 7.744f, 8.256f, 8f, 7.999f)
+              curveTo(8.257f, 7.743f, 8.569f, 7.549f, 8.913f, 7.434f)
+              lineTo(10.304f, 6.983f)
+              curveTo(10.456f, 6.929f, 10.594f, 6.84f, 10.706f, 6.724f)
+              curveTo(10.817f, 6.608f, 10.901f, 6.467f, 10.949f, 6.313f)
+              curveTo(10.998f, 6.159f, 11.01f, 5.996f, 10.985f, 5.837f)
+              curveTo(10.96f, 5.677f, 10.898f, 5.526f, 10.804f, 5.394f)
+              curveTo(10.67f, 5.208f, 10.479f, 5.071f, 10.26f, 5.003f)
+              lineTo(8.885f, 4.556f)
+              curveTo(8.541f, 4.442f, 8.228f, 4.249f, 7.971f, 3.993f)
+              curveTo(7.714f, 3.736f, 7.52f, 3.424f, 7.405f, 3.079f)
+              lineTo(6.953f, 1.691f)
+              curveTo(6.881f, 1.489f, 6.748f, 1.314f, 6.571f, 1.191f)
+              curveTo(6.439f, 1.098f, 6.286f, 1.036f, 6.125f, 1.012f)
+              curveTo(5.965f, 0.987f, 5.801f, 1.001f, 5.646f, 1.051f)
+              curveTo(5.492f, 1.101f, 5.351f, 1.187f, 5.236f, 1.301f)
+              curveTo(5.12f, 1.415f, 5.033f, 1.555f, 4.98f, 1.708f)
+              lineTo(4.523f, 3.108f)
+              curveTo(4.409f, 3.443f, 4.22f, 3.748f, 3.97f, 3.999f)
+              curveTo(3.721f, 4.25f, 3.418f, 4.441f, 3.083f, 4.557f)
+              lineTo(1.692f, 5.005f)
+              curveTo(1.541f, 5.06f, 1.404f, 5.149f, 1.292f, 5.265f)
+              curveTo(1.18f, 5.381f, 1.097f, 5.521f, 1.048f, 5.675f)
+              curveTo(1f, 5.829f, 0.988f, 5.992f, 1.013f, 6.151f)
+              curveTo(1.038f, 6.31f, 1.099f, 6.462f, 1.192f, 6.593f)
+              curveTo(1.32f, 6.773f, 1.501f, 6.908f, 1.709f, 6.979f)
+              lineTo(3.083f, 7.424f)
+              curveTo(3.524f, 7.571f, 3.91f, 7.845f, 4.193f, 8.212f)
+              curveTo(4.356f, 8.423f, 4.481f, 8.66f, 4.564f, 8.912f)
+              lineTo(5.016f, 10.303f)
+              curveTo(5.088f, 10.507f, 5.222f, 10.683f, 5.398f, 10.807f)
+              close()
+              moveTo(11.535f, 14.849f)
+              curveTo(11.671f, 14.946f, 11.834f, 14.997f, 12f, 14.997f)
+              curveTo(12.165f, 14.997f, 12.326f, 14.946f, 12.461f, 14.851f)
+              curveTo(12.601f, 14.753f, 12.706f, 14.613f, 12.761f, 14.451f)
+              lineTo(13.009f, 13.689f)
+              curveTo(13.063f, 13.531f, 13.152f, 13.387f, 13.269f, 13.268f)
+              curveTo(13.387f, 13.15f, 13.531f, 13.061f, 13.689f, 13.009f)
+              lineTo(14.461f, 12.757f)
+              curveTo(14.619f, 12.703f, 14.756f, 12.6f, 14.852f, 12.464f)
+              curveTo(14.926f, 12.361f, 14.974f, 12.242f, 14.992f, 12.117f)
+              curveTo(15.011f, 11.992f, 14.999f, 11.865f, 14.959f, 11.745f)
+              curveTo(14.918f, 11.625f, 14.85f, 11.516f, 14.76f, 11.428f)
+              curveTo(14.669f, 11.34f, 14.559f, 11.274f, 14.438f, 11.236f)
+              lineTo(13.674f, 10.987f)
+              curveTo(13.516f, 10.935f, 13.372f, 10.846f, 13.254f, 10.729f)
+              curveTo(13.136f, 10.611f, 13.047f, 10.467f, 12.994f, 10.309f)
+              lineTo(12.742f, 9.536f)
+              curveTo(12.689f, 9.379f, 12.586f, 9.242f, 12.449f, 9.146f)
+              curveTo(12.347f, 9.073f, 12.23f, 9.025f, 12.106f, 9.006f)
+              curveTo(11.982f, 8.987f, 11.855f, 8.998f, 11.736f, 9.037f)
+              curveTo(11.616f, 9.076f, 11.508f, 9.142f, 11.419f, 9.231f)
+              curveTo(11.33f, 9.319f, 11.264f, 9.427f, 11.224f, 9.546f)
+              lineTo(10.977f, 10.308f)
+              curveTo(10.925f, 10.466f, 10.838f, 10.61f, 10.721f, 10.728f)
+              curveTo(10.607f, 10.845f, 10.467f, 10.934f, 10.312f, 10.987f)
+              lineTo(9.539f, 11.239f)
+              curveTo(9.38f, 11.293f, 9.242f, 11.396f, 9.145f, 11.533f)
+              curveTo(9.047f, 11.669f, 8.995f, 11.833f, 8.996f, 12.001f)
+              curveTo(8.997f, 12.169f, 9.051f, 12.333f, 9.15f, 12.468f)
+              curveTo(9.249f, 12.604f, 9.388f, 12.705f, 9.547f, 12.757f)
+              lineTo(10.31f, 13.004f)
+              curveTo(10.469f, 13.058f, 10.614f, 13.147f, 10.732f, 13.265f)
+              curveTo(10.851f, 13.384f, 10.939f, 13.528f, 10.99f, 13.687f)
+              lineTo(11.243f, 14.461f)
+              curveTo(11.298f, 14.618f, 11.4f, 14.753f, 11.535f, 14.849f)
+              close()
+            }
+      }
+      .build()
+}
