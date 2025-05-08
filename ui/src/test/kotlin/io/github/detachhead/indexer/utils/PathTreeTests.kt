da6564cc64e5936e97fc9f6cb44a9183920727d9
detachhead <detachhead@users.noreply.github.com>
2025-05-08 13:53:25 +1000

    fix `pathTree` unit test

--- ui/src/test/kotlin/io/github/detachhead/indexer/utils/PathTreeTests.kt
+++ ui/src/test/kotlin/io/github/detachhead/indexer/utils/PathTreeTests.kt
@@ -12,20 +12,20 @@ class PathTreeTests {
         listOf(Path("foo/baz"), Path("foo/bar/qux"), Path("foo/bar/asdf"), Path("foo/a/b/c"))
     assert(
         pathTree(rootPath, paths.toSet()) ==
-            PathTree(
-                rootPath,
-                setOf(
-                    PathTree(
-                        Path("foo/bar"),
-                        setOf(
-                            PathTree(Path("foo/bar/qux"), emptySet()),
-                            PathTree(Path("foo/bar/asdf"), emptySet()))),
-                    PathTree(Path("foo/baz"), emptySet()),
-                    PathTree(
-                        Path("foo/a"),
-                        setOf(
-                            PathTree(
-                                Path("foo/a/b"),
-                                setOf(PathTree(Path("foo/a/b/c"), emptySet()))))))))
+            PathTree(rootPath) {
+              setOf(
+                  PathTree(Path("foo/bar")) {
+                    setOf(
+                        PathTree(Path("foo/bar/qux")) { emptySet() },
+                        PathTree(Path("foo/bar/asdf")) { emptySet() })
+                  },
+                  PathTree(Path("foo/baz")) { emptySet() },
+                  PathTree(Path("foo/a")) {
+                    setOf(
+                        PathTree(Path("foo/a/b")) {
+                          setOf(PathTree(Path("foo/a/b/c")) { emptySet() })
+                        })
+                  })
+            })
   }
 }
